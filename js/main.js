19c46b6e00b9051280db0e3b2115bf5970bc005c
othwsav <54482114+othwsav@users.noreply.github.com>
2021-02-21 21:07:16 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let $ = e => document.querySelector(e),
+    $$ = e => document.querySelectorAll(e),
+    topOff = e => e.getBoundingClientRect().top,
+    WH = window.innerHeight;
+
+// get siblings function
+let getSiblings =  e => {
+    let siblings = [];
+    if(!e.parentNode) {
+        return siblings;
+    }
+    let sibling  = e.parentNode.firstChild;
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== e) {
+            siblings.push(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+    return siblings;
+};
+
+// scroll to section when clicking on nav link
+[...$$(".nav_links a")].map(el => {
+    el.addEventListener('click', e => {
+        $(`#${e.target.getAttribute("data-to")}`).scrollIntoView({ behavior: 'smooth' });
+    })
+})
+
+// scroll event that watch fixed nav and sync nav links with the scrolled position
+window.addEventListener("scroll", e => {
+    if(window.scrollY > 700)
+        $(".fixed").classList.add("show")
+    else
+        $(".fixed").classList.remove("show");
+
+
+    let idsOf =[...$$("section")].filter(el => topOff(el) > -(WH/2)).map(el => el.id);
+    
+
+
+    getSiblings($(`[data-to="${idsOf[0]}"]`)).map(e => {
+        e.classList.remove("active")
+    })
+    $(`[data-to="${idsOf[0]}"]`).classList.add("active")
+
+    
+})
